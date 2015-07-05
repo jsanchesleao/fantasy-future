c573135ea42341c7900f85fe5050c80a934b6ab1
jsanchesleao <jsanchesleao@gmail.com>
2015-07-05 12:44:56 +0000

    map working

--- src/main.js
+++ src/main.js
@@ -1,2 +1,17 @@
-export default function() {
+var identity = x => x;
+
+var comp = (f, g) => x => f(g(x));
+
+export default function Future(action, mappingFn = identity) {
+  this.action = action;
+  this.mappingFn = mappingFn;
+};
+
+Future.prototype.fork = function(err, succ) {
+  this.action(err, 
+              r => succ(this.mappingFn(r)));
+};
+
+Future.prototype.map = function(fn) {
+  return new Future(this.action, comp(fn, this.mappingFn));
 };
--- test/forkSpec.js
+++ test/forkSpec.js
@@ -4,7 +4,49 @@ import Future from '../src/main';
 let {expect} = chai;
 
 describe('FORK', function() {
-  it('compiles', function(){
-    expect(Future).to.exist
+  it('executes passed function only after a fork has been called', function(done){
+    var executed = false;
+    var f = new Future(function(reject, resolve) {
+      executed = true;
+      resolve(1);
+    });
+
+    expect(executed).to.equal(false);
+
+    f.fork(function(e){ throw new Error('Error'); },
+           function(data) {
+             expect(data).to.equal(1);
+             expect(executed).to.equal(true);
+             done();
+           });
+  });
+
+  it('maps with a function and returns another future, linking the forks', function(done) {
+    var f = new Future(function(reject, resolve) {
+      resolve(1);
+    });
+
+    var f2 = f.map((x) => x + 1);
+
+    f2.fork((e) => {throw new Error('Error');},
+            (data) => {
+              expect(data).to.equal(2);
+              done();
+            });
+  });
+
+  it('can map arbitrarily', function(done) {
+    var f = new Future(function(reject, resolve) {
+      resolve(1);
+    });
+
+    var f2 = f.map(x => x + 1)
+              .map(x => x * 2);
+
+    f2.fork(e => {throw new Error('Error');},
+            data => {
+              expect(data).to.equal(4);
+              done();
+            });
   });
 });
